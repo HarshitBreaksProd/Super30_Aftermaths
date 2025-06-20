6ebe26f5a9c401a964bee4c02db05cc6ab454c3e
HarshitCodes2 <g1harshit7@gmail.com>
2025-06-20 06:29:59 +0530

    Meetdraw v1 complete

---
+++ Projects/Cohort3/meetdraw_old/packages/db/index.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from "./app/prisma/client";
+
+const globalForPrisma = global as unknown as { client: PrismaClient };
+
+const client = globalForPrisma.client || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.client = client;
+export default client;
