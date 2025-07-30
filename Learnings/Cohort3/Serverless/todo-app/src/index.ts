ead302755f30f322cb339b2360e44edc3641291c
HarshitCodes2 <g1harshit7@gmail.com>
2025-07-30 13:57:12 +0530

    Cloudflare Workers learnt

---
+++ Learnings/Cohort3/Serverless/todo-app/src/index.ts
@@ -0,0 +1,13 @@
+// export default {
+// 	async fetch(request, env, ctx): Promise<Response> {
+// 		return new Response('Hello World!');
+// 	},
+// } satisfies ExportedHandler<Env>;
+
+const main: ExportedHandler<Env> = {
+  async fetch(request, env, ctx): Promise<Response> {
+    return new Response("Hello World");
+  },
+};
+
+export default main;
